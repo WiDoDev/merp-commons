066947b9b02dac73f66e3f13d7062b12e067ce72
Alberto Guerrero <guerrero.fullstack@gmail.com>
2022-02-23 01:19:04 +0100

    Changed currentUser middleware to work with headers instead of sessions

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { verify } from 'jsonwebtoken';
 
 interface UserPayload {
-  id: string;
+  userId: string;
   email: string;
 }
 
@@ -14,22 +14,28 @@ declare global {
   }
 }
 
-export const currentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (!req.session?.jwt) {
-    return next();
-  }
-
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
-    ) as UserPayload;
-    req.currentUser = payload;
-  } catch (err) {}
-
-  next();
-};
+export const currentUser = (JWTKey: string) => {
+  return (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    
+    if (!req.headers?.authorization) {
+      return next();
+    }
+  
+    try {
+      console.log('HEADERS', req.headers);
+      const token = req.headers?.authorization.replace('Bearer ', '');
+      console.log('JWT', token);
+      const payload = verify(
+        token,
+        JWTKey,
+      ) as UserPayload;
+      req.currentUser = payload;
+    } catch (err) {}
+  
+    next();
+  };
+}
